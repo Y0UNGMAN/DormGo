fc43d3f580b00abdcc041e39efe926bf4ad3b17f
Y0UNGMAN <2324804308@qq.com>
2025-11-05 23:53:53 +0800

    Add 数据库 1.2

--- backend/model/dg_post.go
+++ backend/model/dg_post.go
@@ -1,5 +1,16 @@
 package model
 
-type dg_post struct {
-	ID uint `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
+type dgPost struct {
+	ID          uint   `gorm:"primarykey;AUTO_INCREMENT" json:"id"`
+	PublisherId uint   `gorm:"column:publisherid" json:"publisherid"`
+	AccepterId  uint   `gorm:"column:accepterid" json:"accepterid"`
+	DormId      uint   `gorm:"column:dormid" json:"dormid"`
+	Dorm        dgDorm `gorm:"foreignkey:DormId;references:DormId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" `
+	Title       string `gorm:"column:title;type:varchar(255);" json:"title"`
+	Content     string `gorm:"column:content;type:text;" json:"content"`
+	TypeId      uint   `gorm:"column:typeid" json:"typeid"`
+	Type        dgType `gorm:"foreignkey:TypeId;references:TypeId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL" `
+	Status      string `gorm:"column:status;type:varchar(50);" json:"status"`
+	CreatedAt   int64  `gorm:"column:created_at" json:"created_at"`
+	UpdatedAt   int64  `gorm:"column:updated_at" json:"updated_at"`
 }
--- backend/model/init.go
+++ backend/model/init.go
@@ -18,18 +18,30 @@ func Database() {
 		println("数据库连接成功")
 	}
 	//创建用户表
-	err = db.AutoMigrate(&dg_user{})
+	err = db.AutoMigrate(&dgUser{})
 	if err != nil {
 		println("创建用户表失败")
 	} else {
 		println("创建用户表成功")
 	}
 	//创建post表
-	err = db.AutoMigrate(&dg_post{})
+	err = db.AutoMigrate(&dgPost{})
 	if err != nil {
 		println("创建post表失败")
 	} else {
 		println("创建post表成功")
+	}
+	err = db.AutoMigrate(&dgDorm{})
+	if err != nil {
+		println("创建dorm表失败")
+	} else {
+		println("创建dorm表成功")
+	}
+	err = db.AutoMigrate(&dgType{})
+	if err != nil {
+		println("创建type表失败")
+	} else {
+		println("创建type表成功")
 	}
 	DB = db
 
