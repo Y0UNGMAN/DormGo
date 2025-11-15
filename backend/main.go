5babbdff0821997f4ac9559f675c24391348fc5e
Y0UNGMAN <2324804308@qq.com>
2025-11-15 11:17:42 +0800

    1.0

--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"github.com/Y0UNGMAN/DormGo/backend/model"
+	"github.com/Y0UNGMAN/DormGo/backend/redis"
 	"github.com/Y0UNGMAN/DormGo/backend/router"
 	"github.com/Y0UNGMAN/DormGo/backend/settings"
 )
@@ -22,6 +23,10 @@ func main() {
 		return
 	}
 	//4.初始化Redis连接
+	if err := redis.Init(); err != nil {
+		fmt.Printf("init redis failed, err:%v\n", err)
+		return
+	}
 	//5.注册路由
 	r := router.App()
 	//6.启动服务
